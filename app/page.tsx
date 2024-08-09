e59b6cb23ff867b560e85a992871ec42054eea89
hyperst8 <huypn82@gmail.com>
2024-08-09 15:21:54 +0200

    Query data and display on home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,53 @@
-export default function Home() {
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import Image from "next/image";
+import Link from "next/link";
+import { simpleBlogCard } from "./lib/interface";
+import { client, urlFor } from "./lib/sanity";
+
+async function getData() {
+  const query = `
+    *[_type == 'blog'] | order(_createdAt desc) {
+    title,
+      smallDescription,
+      "currentSlug": slug.current,
+      titleImage
+  }
+  `;
+
+  const data = await client.fetch(query);
+
+  return data;
+}
+
+export default async function Home() {
+  const data: simpleBlogCard[] = await getData();
+
   return (
-    <div>
-      <h1>Hola</h1>
+    <div className="grid grid-cols-1 md:grid-cols-2 mt-5 gap-5">
+      {data.map((post, idx) => (
+        <Card key={idx}>
+          <Image
+            src={urlFor(post.titleImage).url()}
+            alt={post.title}
+            width={500}
+            height={500}
+            className="rounded-t-lg h-[200px] object-cover"
+          />
+          <CardContent className="mt-5">
+            <h2 className="text-lg font-bold line-clamp-2">{post.title}</h2>
+            <p className="line-clamp-3 text-sm mt-2 text-gray-600 dark:text-gray-300">
+              {post.smallDescription}
+            </p>
+            <Button
+              asChild
+              className="w-full mt-7"
+            >
+              <Link href={`/blog/${post.currentSlug}`}>Read More</Link>
+            </Button>
+          </CardContent>
+        </Card>
+      ))}
     </div>
   );
 }
