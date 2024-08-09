64b0fadb28fe58a29fb07dc3f8832583f4e0b936
hyperst8 <huypn82@gmail.com>
2024-08-09 21:56:16 +0200

    Create page template for specific blog

---
+++ app/blog/[slug]/page.tsx
@@ -0,0 +1,47 @@
+import { fullBlog } from "@/app/lib/interface";
+import { client, urlFor } from "@/app/lib/sanity";
+import { PortableText } from "next-sanity";
+import Image from "next/image";
+
+async function getData(slug: string) {
+  const query = `
+    *[_type == "blog" && slug.current == '${slug}'] {
+        "currentSlug": slug.current,
+        title,
+        content,
+        titleImage
+        }[0]
+    `;
+  const data = await client.fetch(query);
+
+  return data;
+}
+
+export default async function BlogPost({
+  params,
+}: {
+  params: { slug: string };
+}) {
+  const data: fullBlog = await getData(params.slug);
+
+  return (
+    <div className="mt-8">
+      <h1 className="mt-8 block text-3xl text-center leading-8 font-bold tracking-tight sm:text-4xl">
+        {data.title}
+      </h1>
+
+      <Image
+        src={urlFor(data.titleImage).url()}
+        alt={data.title}
+        width={800}
+        height={800}
+        priority
+        className="mt-8 rounded-lg border"
+      />
+
+      <div className="mt-16 prose prose-blue prose-xl dark:prose-invert prose-li:marker:text-primary">
+        <PortableText value={data.content} />
+      </div>
+    </div>
+  );
+}
